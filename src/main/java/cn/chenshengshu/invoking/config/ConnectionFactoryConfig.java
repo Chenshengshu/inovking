ca73eb0dc7a9902a86a9c1590b27b5fb437b7067
chenshengshu <827895431@qq.com>
2021-01-09 22:01:27 +0800

    修改：简化配置

--- src/main/java/cn/chenshengshu/invoking/config/ConnectionFactoryConfig.java
+++ src/main/java/cn/chenshengshu/invoking/config/ConnectionFactoryConfig.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,24 +20,10 @@ import java.util.concurrent.TimeoutException;
 @Configuration
 public class ConnectionFactoryConfig {
 
-    @Value("${spring.rabbitmq.host}")
-    private String host;
-    @Value("${spring.rabbitmq.port}")
-    private Integer port;
-    @Value("${spring.rabbitmq.username}")
-    private String username;
-    @Value("${spring.rabbitmq.password}")
-    private String password;
-
-
+    @ConfigurationProperties("spring.rabbitmq")
     @Bean(value = "interfaceFactory")
     public ConnectionFactory getConnectionFactory() {
         ConnectionFactory factory = new ConnectionFactory();
-        //设置登录账号
-        factory.setHost(host);
-        factory.setPort(port);
-        factory.setUsername(username);
-        factory.setPassword(password);
         return factory;
     }
 
